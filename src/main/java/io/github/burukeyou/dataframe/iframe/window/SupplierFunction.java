64e4266f0eac9c7b100e765e75c51476e69a41c0
caizhihao <burukeyou@163.com>
2024-05-06 13:41:11 +0800

    refactor: remove class

--- src/main/java/io/github/burukeyou/dataframe/iframe/window/SupplierFunction.java
+++ src/main/java/io/github/burukeyou/dataframe/iframe/window/SupplierFunction.java
@@ -4,6 +4,11 @@ import io.github.burukeyou.dataframe.iframe.item.FI2;
 
 import java.util.List;
 
+/**
+ * @author  caizhihao
+ * @param <T>
+ * @param <V>
+ */
 public interface SupplierFunction<T,V> {
 
     List<FI2<T, V>> get(List<T> windowList);
